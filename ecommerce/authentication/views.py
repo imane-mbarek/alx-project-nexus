497c6b0bae40d9c86d6644b9e895dfb6370c323c
imane AIT MBAREK <imane.aytmbarek@gmail.com>
2025-08-03 18:57:00 +0100

    Update views.py

--- ecommerce/authentication/views.py
+++ ecommerce/authentication/views.py
@@ -11,7 +11,6 @@ from rest_framework.authtoken.models import Token
 from .serializers import (
     RegisterSerializer, LoginSerializer, UserProfileSerializer, ResetPasswordSerializer
 )
-from django.http import JsonResponse
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
@@ -61,5 +60,3 @@ class ResetPasswordView(generics.GenericAPIView):
         return Response({"message": "Password reset link sent"}, status=status.HTTP_200_OK)
 
 
-def home(request):
-    return JsonResponse({"message": "Welcome to Project Nexus API."})
